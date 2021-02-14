3bca441af580302278b6e7caae76842469af0ade
mnao305 <mail@mnao305.com>
2021-02-14 19:15:00 +0900

    feat(types): add World Objects type

---
+++ src/types/World.ts
@@ -0,0 +1,95 @@
+import { WorldUnityPackages } from './UnityPackage'
+
+type ReleaseStatus = 'public' | 'private' | 'hidden'
+type Instance = [string, number]
+type InstanceType = 'hidden' | 'friends' | 'public'
+
+interface Platforms {
+  standalonewindows: number
+  android: number
+}
+
+export interface WorldObject {
+  assetUrl: string
+  assetUrlObject: {}
+  authorId: string
+  authorName: string
+  capacity: number // ? API docではstringになってる
+  created_at: string
+  description: string
+  favorites: number
+  featured: boolean
+  heat: number
+  id: string
+  imageUrl: string
+  instances: Instance[]
+  labsPublicationDate: string
+  name: string
+  namespace: string
+  occupants: number
+  organization: string
+  pluginUrl: string
+  pluginUrlObject: {}
+  popularity: number
+  previewYoutubeId: null | any // ? 不明。API docに存在していない
+  privateOccupants: number // ? API docに存在していない
+  publicOccupants: number
+  publicationDate: string
+  releaseStatus: ReleaseStatus
+  tags: string[]
+  thumbnailImageUrl: string
+  unityPackageUrl: string
+  unityPackageUrlObject: {}
+  unityPackages: WorldUnityPackages[]
+  updated_at: string
+  version: number
+  visits: number
+}
+
+export interface LimitedWorldObject {
+  authorId: string
+  authorName: string
+  capacity: number
+  created_at: string
+  favorites: number
+  heat: number
+  id: string
+  imageUrl: string
+  labsPublicationDate: string
+  name: string
+  occupants: number
+  organization: string
+  popularity: number
+  publicationDate: string
+  releaseStatus: ReleaseStatus
+  tags: string[]
+  thumbnailImageUrl: string
+  unityPackages: WorldUnityPackages[]
+  updated_at: string
+  visits: number
+}
+
+export interface LimitedFavoriteWorldObject extends LimitedWorldObject {
+  favoriteId?: string
+}
+
+export interface InstanceObject {
+  active: boolean
+  canRequestInvite: boolean
+  capacity: number
+  clientNumber: string
+  full: boolean
+  id: string
+  instanceId: string
+  location: string
+  n_users: number
+  name: string
+  ownerId: string
+  permanent: boolean
+  photonRegion: string
+  platforms: Platforms
+  shortName: string
+  tags: string[]
+  type: InstanceType
+  worldId: string
+}
