170650b034bb78fb1ee58ae51aeb0f610443ebed
mnao305 <mail@mnao305.com>
2021-02-22 05:53:36 +0900

    chore: add jsdoc (#13)

--- src/System/Config.ts
+++ src/System/Config.ts
@@ -1,6 +1,10 @@
 import { ConfigObject } from 'src/types/Config'
 import { get } from '../util/API'
 
+/**
+ * get the configuration that the clients needs to work properly
+ * https://vrchatapi.github.io/#/SystemAPI/Config
+ */
 export const config = async (): Promise<ConfigObject> => {
   const tmp = await get<ConfigObject>('/config')
   return tmp
