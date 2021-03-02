f7f3fbe7adb041da6a3f4d42b9a4a2ad37cc2aab
mnao305 <mail@mnao305.com>
2021-03-02 10:33:08 +0900

    docs: add jsdoc

--- src/User/Logout.ts
+++ src/User/Logout.ts
@@ -2,6 +2,10 @@ import { put } from '../util/API'
 
 export interface logoutMsg {'success': {'message': 'Ok!', 'status_code': 200}}
 
+/***
+ * invalidates auth cookie.
+ * https://vrchatapi.github.io/#/UserAPI/Logout
+ */
 export const logout = async (): Promise<logoutMsg> => {
   const msg = await put<logoutMsg>('/logout')
   return msg
