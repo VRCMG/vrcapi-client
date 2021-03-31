54ef00ef9de4988e38e5044770d81155eb6926aa
mnao305 <mail@mnao305.com>
2021-03-31 10:45:54 +0900

    refactor(util): set cookies use axios features (#78)

--- src/util/API.ts
+++ src/util/API.ts
@@ -5,13 +5,8 @@ axios.defaults.baseURL = 'https://api.vrchat.cloud/api/1'
 axios.defaults.withCredentials = true
 axios.defaults.headers.common['Content-Type'] = 'application/json'
 
-let cookie = ''
-
 export const get = async <T>(path: string, params?: Record<string, unknown>): Promise<T> => {
   const res = await axios.get<T>(path, {
-    headers: {
-      Cookie: cookie
-    },
     params
   })
 
@@ -20,24 +15,16 @@ export const get = async <T>(path: string, params?: Record<string, unknown>): Pr
 
 export const getAuth = async <T>(path: string, apiKey: string, username: string, password: string): Promise<T> => {
   const res = await axios.get<T>(path, { params: { apiKey }, auth: { username, password } })
-  cookie = res.headers['set-cookie']
+  axios.defaults.headers.common.Cookie = res.headers['set-cookie']
   return res.data
 }
 
 export const post = async <T>(path: string, data: Record<string, unknown>): Promise<T> => {
-  const res = await axios.post<T>(path, data, {
-    headers: {
-      Cookie: cookie
-    }
-  })
+  const res = await axios.post<T>(path, data)
   return res.data
 }
 
 export const put = async <T>(path: string, data: Record<string, unknown> = {}): Promise<T> => {
-  const res = await axios.put<T>(path, data, {
-    headers: {
-      Cookie: cookie
-    }
-  })
+  const res = await axios.put<T>(path, data)
   return res.data
 }
